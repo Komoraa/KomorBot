64fd3b4ac4118b43ff0b285d8f47a1bddf79e4de
Komoraa <82817981+Komoraa@users.noreply.github.com>
2021-06-28 01:30:49 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,96 @@
+import asyncio
+import os
+import discord
+from discord_slash import SlashCommand # Importing the newly installed library.
+from discord.ext import commands
+import random
+from tablice import *
+from config import *
+
+bot = commands.Bot(command_prefix="!")
+slash = SlashCommand(bot, sync_commands=True) # Declares slash commands through the client.
+guild_ids = [572823253798617109]
+prefix = '/'
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game(name='Wpisz ' + prefix + 'pomoc'))
+    print('Bot {0.user} działa jak natura chciała'.format(bot))
+
+
+@slash.slash()
+async def pomoc(ctx):
+    await ctx.send(prefix + 'siema - no elo byczku skąd to zwątpienie \n'
+                   + prefix + 'minecraftlava - minecraftlava \n'
+                   + prefix + 'gra - zagrajmy w gre \n'
+                   + prefix + 'roll - rolluj w bannerze standardowym genshina')
+
+
+@slash.slash()
+async def siema(ctx):
+    await ctx.send('Cześć Przyjacielu ' + ctx.author.mention)
+    await ctx.send(
+        'https://cdn.discordapp.com/attachments/572823794343870464/849747028632010812/1498940958_1aa.gif')
+
+
+@slash.slash()
+async def roll(ctx):
+    czyqi=random.randint(1, 1000)
+    if czyqi<=6:
+        await ctx.send('Gratulacje ' + ctx.author.mention
+                   + ' wylosowałeś Qiqi ' + str(discord.utils.get(bot.emojis, name='megabeka')))
+        await ctx.send(
+        'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1a2212b9-af4a-4302-9499-08be7aaa7ff2/de8t8yw-ee0a8eec-00b6-4f09-afd1-89039e9643f5.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzFhMjIxMmI5LWFmNGEtNDMwMi05NDk5LTA4YmU3YWFhN2ZmMlwvZGU4dDh5dy1lZTBhOGVlYy0wMGI2LTRmMDktYWZkMS04OTAzOWU5NjQzZjUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.v5QiSeP_kzLfU2qaOlR5smy8FHKvry-AsQJxHfGOI8E')
+    else:
+        los=random.randint(0, 14)
+        await ctx.send('Gratulacje ' + ctx.author.mention
+                       + ' wylosowałeś ' + Genshin_nazwa[los])
+        await ctx.send(Genshin_link[los])
+
+
+@slash.slash()
+async def minecraftlava(ctx):
+    await ctx.send(str(discord.utils.get(bot.emojis, name='minecraft_lava')))
+
+
+@slash.slash()
+async def gra(ctx):
+    channel = ctx.channel
+    await channel.send('Zagrajmy w gre. Podaj liczbe.')
+
+    try:
+        msg = await bot.wait_for('message', timeout=30.0, check=lambda message: message.author == ctx.author)
+        await channel.send(str(int(msg.content) + 1))
+        await channel.send('przegrałeś')
+    except asyncio.TimeoutError:
+        await channel.send('Chcesz mi komputer ugotować?')
+    except ValueError:
+        await channel.send('Liczba debilu wiesz co to znaczy?')
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if any(word in message.content for word in DST):
+        await message.reply(random.choice(DST_Reakcja), mention_author=True)
+
+    if any(word in message.content for word in CZTERY):
+        await message.add_reaction((discord.utils.get(bot.emojis, name='Jhin4')))
+
+    if any(word in message.content for word in PENTA):
+        f = open("penta.txt", "r")
+        licznik=int(f.read())
+        licznik+=1
+        f.close()
+        await message.channel.send('Where penta?: ' + str(licznik))
+        await message.channel.send('https://cdn.discordapp.com/attachments/572823794343870464/852594093895843880'
+                                   '/penta.png')
+        f = open("penta.txt", "w")
+        f.write(str(licznik))
+        f.close()
+
+    await bot.process_commands(message)
+
+bot.run(token)
---
+++ tablice.py
@@ -0,0 +1,34 @@
+DST = ["DST", "dst", "Dont Starve", "dont starve", "Don't Starve", "don't starve","Dont starve","Don't starve"]
+
+DST_Reakcja = ["https://tenor.com/view/kekwtf-gif-18599263"
+    , "https://media.discordapp.net/attachments/572823741168484364/832237422464991232/6976_OMEGAWHEELCHAIR.gif"
+    , "https://tenor.com/view/laughing-spiderman-simmons-jonah-jameson-gif-18832432"
+    , "https://tenor.com/view/pepe-laugh-kek-intensifies-kek_intensifies-gif-10111909"
+    , "https://tenor.com/view/kekcrab-crabkek-crabkekw-kekwceab-crab-gif-19258334"]
+
+CZTERY=["4","cztery","Cztery"]
+
+PENTA=['Penta','penta','PENTA','Pentakill','pentakill','PENTAKILL']
+
+Genshin_nazwa=['Magic Guide', 'Black Tassel', 'Bloodtainted Greatsword', 'Cool Steel', 'Debate Club',
+               'Raven Bow', 'Emerald Orb', 'Sharpshooter\'s Oath', 'Skyrider Greatsword', 'Skyrider Sword',
+               'Slingshot', 'Ferrous Shadow', 'Fillet Blade', 'Thrilling Tales of Dragon Slayers',
+               'Harbinger of Dawn']
+
+Genshin_link=\
+    ['https://playpc.io/wp-content/uploads/2020/12/genshin-impact-wish.jpeg',
+     'https://i.ytimg.com/vi/vAKrXcDsmwU/maxresdefault.jpg',
+     'https://oyster.ignimgs.com/mediawiki/apis.ign.com/genshin-impact/1/18/Genshin_Impact_20200929215301.jpg?width=1280',
+     'https://i.ytimg.com/vi/vHi4S8GhTYo/maxresdefault.jpg',
+     'https://i.ytimg.com/vi/mZ3skOgoPGE/maxresdefault.jpg',
+     'https://cdn.discordapp.com/attachments/572823794343870464/850770810871480340/unknown.png',
+     'https://static.wikia.nocookie.net/gensin-impact/images/7/7c/Weapon_Emerald_Orb.png/revision/latest/scale-to-width-down/512?cb=20201120003056',
+     'https://static.wikia.nocookie.net/gensin-impact/images/8/8d/Weapon_Sharpshooter%27s_Oath_3D.png/revision/latest/scale-to-width-down/130?cb=20201011015146',
+     'https://static.wikia.nocookie.net/gensin-impact/images/2/2d/Weapon_Skyrider_Greatsword_3D.png/revision/latest/scale-to-width-down/132?cb=20201010144836',
+     'https://static.wikia.nocookie.net/gensin-impact/images/4/45/Weapon_Skyrider_Sword_3D.png/revision/latest/scale-to-width-down/121?cb=20201010041444',
+     'https://static.wikia.nocookie.net/gensin-impact/images/c/cc/Weapon_Slingshot_3D.png/revision/latest/scale-to-width-down/127?cb=20201208025041',
+     'https://static.wikia.nocookie.net/gensin-impact/images/6/64/Weapon_Ferrous_Shadow_3D.png/revision/latest/scale-to-width-down/118?cb=20201010134436',
+     'https://static.wikia.nocookie.net/gensin-impact/images/c/cc/Weapon_Fillet_Blade_3D.png/revision/latest/scale-to-width-down/129?cb=20201011151857',
+     'https://static.wikia.nocookie.net/gensin-impact/images/1/19/Weapon_Thrilling_Tales_of_Dragon_Slayers.png/revision/latest/scale-to-width-down/512?cb=20201119201736',
+     'https://static.wikia.nocookie.net/gensin-impact/images/2/23/Weapon_Harbinger_of_Dawn_3D.png/revision/latest/scale-to-width-down/131?cb=20201010022249'
+     ]
